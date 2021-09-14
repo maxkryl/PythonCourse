c46666df428656dd869adbfc4f6947d417483dec
maxkryl <ackpunuk@bk.ru>
2021-09-14 23:33:58 +0300

    Kek

--- Work/mortgage2.py
+++ Work/mortgage2.py
@@ -1,18 +1,21 @@
 principal = 5e5
 rate = 5e-2
 payment = 2684.11
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
 total_paid = 0.0
 i = 1
 
 while principal > 0:
 
-    if i <= 12:
-        principal = principal * (1 + rate/12) - (payment + 1000)
+    if i >= extra_payment_start_month and i <= extra_payment_end_month:
+        principal = principal * (1 + rate/12) - (payment + extra_payment)
 
         if principal < 0:
             payment = payment + principal
             
-        total_paid = total_paid + payment + 1000
+        total_paid = total_paid + payment + extra_payment
     else:
         principal = principal * (1 + rate/12) - payment
 
---
+++ Work/mortgage3.py
@@ -0,0 +1,32 @@
+principal = 5e5
+rate = 5e-2
+payment = 2684.11
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+total_paid = 0.0
+i = 1
+
+while principal > 0:
+
+    if i >= extra_payment_start_month and i <= extra_payment_end_month:
+        principal = principal * (1 + rate/12) - (payment + extra_payment)
+
+        if principal < 0:
+            remaning_payment = principal + payment + extra_payment
+            payment = payment + extra_payment + principal
+            
+        total_paid = total_paid + payment + extra_payment
+    else:
+        principal = principal * (1 + rate/12) - payment
+
+        if principal < 0:
+            remaning_payment = principal + payment
+            payment = payment + principal
+
+        total_paid = total_paid + payment
+    i = i + 1
+    if principal < 0:
+        print(i-1, round(total_paid, 2), remaning_payment - payment)
+    else:
+        print(i-1, round(total_paid, 2), round(principal, 2))
